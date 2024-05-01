ffbb459aea3a19472cfc1f44328bf37959ecc32f
DMcGlinchey <166512634+DMcGlinchey@users.noreply.github.com>
2024-05-01 13:39:12 +0100

    Add files via upload

---
+++ Extract.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Compression;
+
+namespace DHL_API_READER
+{
+    public class Extract
+    {
+        public static void Zip(string base64Zip)
+        {
+            string extractPath = @"C:\temp\DHL";
+
+            byte[] zipBytes = Convert.FromBase64String(base64Zip);
+            using var memoryStream = new MemoryStream(zipBytes);
+            using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read);
+            archive.ExtractToDirectory(extractPath);
+        }
+
+        public static void PDF(string base64)
+        {
+            string extractPath = @"C:\temp\DHL\test.pdf";
+
+            byte[] zipBytes = Convert.FromBase64String(base64);
+            using var memoryStream = new MemoryStream(zipBytes);
+            File.WriteAllBytes(extractPath, zipBytes);
+        }
+    }
+}
